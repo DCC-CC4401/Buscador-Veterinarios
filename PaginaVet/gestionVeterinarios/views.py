fa4f7286dea0c9b3db7acae1b177d1eea406b879
Camila <camila.labarca@ug.uchile.cl>
2021-04-25 17:06:57 -0400

    avance

--- PaginaVet/gestionVeterinarios/views.py
+++ PaginaVet/gestionVeterinarios/views.py
@@ -15,10 +15,37 @@ def formVeterinario(request): #the index view
 def formEvaluacion(request):
     if request.method == "GET":
         return render(request, "gestionVeterinarios/formevaluacion.html")
+    elif request.method == "POST":
+        nombre=request.POST["nombre"]
+        if request.POST["estrellas"] == "5":
+            estrellas = 5
+        elif request.POST["estrellas"] == "4":
+            estrellas = 4
+        elif request.POST["estrellas"] == "3":
+            estrellas = 3
+        elif request.POST["estrellas"] == "2":
+            estrellas = 2
+        else:
+            estrellas = 1
+        razon_consulta=request.POST["razon_consulta"]       
+        if request.POST["problema_resuelto"] == "si":
+            problema_resuelto = True
+        else:
+            problema_resuelto = False
+
+        comentario=request.POST["comentario"]
+
+        nueva_resena = Reseña(nombre=nombre, evaluacion=estrellas, problema_resuelto=problema_resuelto, razon_consulta=razon_consulta,comentario=comentario)
+        nueva_resena.save()
+
+        return redirect("/evaluacionVeterinarios")
+
+
 
 def catalogoVeterinarios(request):
     if request.method == "GET":
-        return render(request, "gestionVeterinarios/catalogodoc.html")
+        doctores=Veterinario.objects.all()
+        return render(request, "gestionVeterinarios/catalogodoc.html", {"doctores":doctores})
 
 def perfil(request):
 
